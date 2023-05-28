7e3f712cd726b74e27af9bab3090bbbfd0b33de6
Mark van Jislaaik <markvanjislaaik@gmail.com>
2023-05-28 15:54:56 +0200

    add file list to mailer.html

--- nifty.py
+++ nifty.py
@@ -38,8 +38,10 @@ if __name__ == '__main__':
     if os.path.isdir(args.file_path):
         logger.info(f"Zipping {args.file_path}")
         zipper = FileZipper()
+        files_list = zipper.list_files(args.file_path)
         prepped_file_path = zipper.create_zip(args.file_path, f"{os.path.basename(args.file_path)}.zip")
     else:
+        files_list = [args.file_path]
         prepped_file_path = args.file_path
 
     # File upload and get shareable link
@@ -54,6 +56,13 @@ if __name__ == '__main__':
     expiry_date = expiry_date.strftime("%A, %B %d, %Y")
 
     # Email formatting
+    file_count = len(files_list)
+
+    if file_count != 1:
+        plural = "Files"
+    else:
+        plural = "File"
+
     mail_context = {
         "sender_name": MailerConfig.MAIL_HOST_SENDER_NAME,
         "file_basename": os.path.basename(prepped_file_path),
@@ -61,7 +70,10 @@ if __name__ == '__main__':
         "download_link": link,
         "recipent_email": args.recipient,
         "expiry_date": expiry_date,
-        "file_size_mb": round(os.path.getsize(prepped_file_path) / 1024 / 1024, 2)
+        "file_size_mb": round(os.path.getsize(prepped_file_path) / 1024 / 1024, 2),
+        "files_list": files_list,
+        "file_count": file_count,
+        "plural": plural
     }
 
     logger.info(f"Rendering email template {args.template}")
--- zipper/file_zipper.py
+++ zipper/file_zipper.py
@@ -2,6 +2,16 @@ import zipfile
 import os
 
 class FileZipper:
+
+    def list_files(self, path: str) -> list:
+        files_list = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                rel_path = os.path.relpath(file_path, path)
+                files_list.append(rel_path.replace("\\", "/"))
+        return files_list
+
     def create_zip(self, path: str, zip_path: str) -> str:
         with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
             base_dir = os.path.basename(path)
